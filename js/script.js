3fbfe947ae0a944649141f68da668d8ba6bb9c92
StevenAndrew <stevenandrew2801@gmail.com>
2024-11-28 11:16:34 +0700

    test slider

--- js/script.js
+++ js/script.js
@@ -22,3 +22,10 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+var divisor = document.getElementById("divisor");
+var comparisonWidth = document.getElementById("comparison").clientWidth;
+
+function moveDivisor(e) {
+  divisor.style.width = (e.offsetX * 100 / comparisonWidth) + "%";
+}
