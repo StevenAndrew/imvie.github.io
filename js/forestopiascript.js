035f1d280d981ba4dde4f2f8edf713e75b20804d
StevenAndrew <stevenandrew2801@gmail.com>
2025-01-10 14:16:35 +0700

    add forestopia

--- js/forestopiascript.js
+++ js/forestopiascript.js
@@ -1,18 +1,16 @@
+function slide(){
+    let slideValue = document.getElementById("slider").value;
+    document.getElementById("img2").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+  }
+  
   var modal1 = document.getElementById("myModal1");
   var modal2 = document.getElementById("myModal2");
   var modal3 = document.getElementById("myModal3");
   var modal4 = document.getElementById("myModal4");
-  var modal5 = document.getElementById("myModal5");
-  var modal6 = document.getElementById("myModal6");
-  var modal7 = document.getElementById("myModal7");
-  
   var btn1 = document.getElementById("myBtn1");
   var btn2 = document.getElementById("myBtn2");
   var btn3 = document.getElementById("myBtn3");
   var btn4 = document.getElementById("myBtn4");
-  var btn5 = document.getElementById("myBtn5");
-  var btn6 = document.getElementById("myBtn6");
-  var btn7 = document.getElementById("myBtn7");
   
   btn1.onclick = function() {
     modal1.style.display = "flex";
@@ -30,17 +28,6 @@
     modal4.style.display = "flex";
   }
   
-  btn5.onclick = function() {
-    modal5.style.display = "flex";
-  }
-  btn6.onclick = function() {
-    modal6.style.display = "flex";
-  }
-  btn7.onclick = function() {
-    modal7.style.display = "flex";
-  }
-  
-  
   window.onclick = function(event) {
     if (event.target == modal1) {
       modal1.style.display = "none";
@@ -50,12 +37,6 @@
       modal3.style.display = "none";
     }else if(event.target == modal4) {
       modal4.style.display = "none";
-    }else if(event.target == modal5) {
-      modal5.style.display = "none";
-    }else if(event.target == modal6) {
-      modal6.style.display = "none";
-    }else if(event.target == modal7) {
-      modal7.style.display = "none";
     }
   }
   
