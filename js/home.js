e8a49a7ae2667dc7b5e6ada89f85394c9b8d1f57
StevenAndrew <stevenandrew2801@gmail.com>
2025-02-03 11:34:30 +0700

    update

---
+++ js/home.js
@@ -0,0 +1,21 @@
+var hiddenDivIntDesign = document.getElementById('IntDesignContent');
+var hiddenDivContractor = document.getElementById('ContractorContent');
+var hiddenDivArchitecture = document.getElementById('ArchitectureContent');
+
+document.getElementById('IntDesign').addEventListener('click', function() {
+    hiddenDivIntDesign.classList.add('visible');
+    hiddenDivContractor.classList.remove('visible');
+    hiddenDivArchitecture.classList.remove('visible');
+});
+
+document.getElementById('Contractor').addEventListener('click', function() {
+    hiddenDivIntDesign.classList.remove('visible');
+    hiddenDivContractor.classList.add('visible');
+    hiddenDivArchitecture.classList.remove('visible');
+});
+
+document.getElementById('Architecture').addEventListener('click', function() {
+    hiddenDivIntDesign.classList.remove('visible');
+    hiddenDivContractor.classList.remove('visible');
+    hiddenDivArchitecture.classList.add('visible');
+});
