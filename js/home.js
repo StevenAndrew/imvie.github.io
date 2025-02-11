90eba7f59deaa9947cd4b78284a8dbd4a1c81e43
StevenAndrew <stevenandrew2801@gmail.com>
2025-02-11 11:42:14 +0700

    fix ortho

--- js/home.js
+++ js/home.js
@@ -6,16 +6,19 @@ document.getElementById('IntDesign').addEventListener('click', function() {
     hiddenDivIntDesign.classList.add('visible');
     hiddenDivContractor.classList.remove('visible');
     hiddenDivArchitecture.classList.remove('visible');
+    document.getElementById("hiddenContent").scrollIntoView({behavior: 'smooth'});
 });
 
 document.getElementById('Contractor').addEventListener('click', function() {
     hiddenDivIntDesign.classList.remove('visible');
     hiddenDivContractor.classList.add('visible');
     hiddenDivArchitecture.classList.remove('visible');
+    document.getElementById("hiddenContent").scrollIntoView({behavior: 'smooth'});
 });
 
 document.getElementById('Architecture').addEventListener('click', function() {
     hiddenDivIntDesign.classList.remove('visible');
     hiddenDivContractor.classList.remove('visible');
     hiddenDivArchitecture.classList.add('visible');
+    document.getElementById("hiddenContent").scrollIntoView({behavior: 'smooth'});
 });
