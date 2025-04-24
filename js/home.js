da437ab08875f0df0cc51afce286d40631b608ff
StevenAndrew <stevenandrew2801@gmail.com>
2025-04-24 12:29:49 +0700

    test redirect

--- js/home.js
+++ js/home.js
@@ -3,22 +3,25 @@ var hiddenDivContractor = document.getElementById('ContractorContent');
 var hiddenDivArchitecture = document.getElementById('ArchitectureContent');
 
 document.getElementById('IntDesign').addEventListener('click', function() {
-    hiddenDivIntDesign.classList.add('visible');
-    hiddenDivContractor.classList.remove('visible');
-    hiddenDivArchitecture.classList.remove('visible');
-    document.getElementById("hiddenContent").scrollIntoView({behavior: 'smooth'});
+    // hiddenDivIntDesign.classList.add('visible');
+    // hiddenDivContractor.classList.remove('visible');
+    // hiddenDivArchitecture.classList.remove('visible');
+    // document.getElementById("hiddenContent").scrollIntoView({behavior: 'smooth'});
+    window.location.href = "./interiorDesign.html";
 });
 
 document.getElementById('Contractor').addEventListener('click', function() {
-    hiddenDivIntDesign.classList.remove('visible');
-    hiddenDivContractor.classList.add('visible');
-    hiddenDivArchitecture.classList.remove('visible');
-    document.getElementById("hiddenContent").scrollIntoView({behavior: 'smooth'});
+    // hiddenDivIntDesign.classList.remove('visible');
+    // hiddenDivContractor.classList.add('visible');
+    // hiddenDivArchitecture.classList.remove('visible');
+    // document.getElementById("hiddenContent").scrollIntoView({behavior: 'smooth'});
+    window.location.href = "./contractor.html";
 });
 
 document.getElementById('Architecture').addEventListener('click', function() {
-    hiddenDivIntDesign.classList.remove('visible');
-    hiddenDivContractor.classList.remove('visible');
-    hiddenDivArchitecture.classList.add('visible');
-    document.getElementById("hiddenContent").scrollIntoView({behavior: 'smooth'});
+    // hiddenDivIntDesign.classList.remove('visible');
+    // hiddenDivContractor.classList.remove('visible');
+    // hiddenDivArchitecture.classList.add('visible');
+    // document.getElementById("hiddenContent").scrollIntoView({behavior: 'smooth'});
+    window.location.href = "./architecture.html";
 });
