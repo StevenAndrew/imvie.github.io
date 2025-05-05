85313ebfdd4095e6d6e5eb597afd4a16f5bb6a0d
StevenAndrew <stevenandrew2801@gmail.com>
2025-05-05 13:16:02 +0700

    update lollypop

--- js/lollypopscript.js
+++ js/lollypopscript.js
@@ -1,7 +1,16 @@
+function slide(){
+    let slideValue = document.getElementById("slider").value;
+    document.getElementById("img2").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
+  }
+
 var modal1 = document.getElementById("myModal1");
 var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
 var btn1 = document.getElementById("myBtn1");
 var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
 
 btn1.onclick = function() {
     modal1.style.display = "flex";
@@ -11,11 +20,25 @@ btn2.onclick = function() {
     modal2.style.display = "flex";
 }
 
+btn3.onclick = function() {
+    modal3.style.display = "flex";
+}
+
+btn4.onclick = function() {
+    modal4.style.display = "flex";
+}
+
 
 window.onclick = function(event) {
     if (event.target == modal1) {
         modal1.style.display = "none";
     }else if(event.target == modal2) {
         modal2.style.display = "none";
+    }
+    else if(event.target == modal3) {
+        modal3.style.display = "none";
+    }
+    else if(event.target == modal4) {
+        modal4.style.display = "none";
     }
 }
